ccfd0b08a6ce8fbdf14c88dced9e227fcb0ae000
Phong Vo <vhphong@gmail.com>
2024-01-10 21:26:43 -0600

    Add existing project files to Git

---
+++ src/main/java/com/pnk/githubjenkinsdocker/GithubJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.pnk.githubjenkinsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnk/githubjenkinsdocker/GithubJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnk.githubjenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
