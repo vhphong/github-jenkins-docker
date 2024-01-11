b2e682e6923338531eff121d095640415b01db3b
Phong Vo <vhphong@gmail.com>
2024-01-10 21:38:42 -0600

    formatted pom.xml

--- src/main/java/com/pnk/githubjenkinsdocker/GithubJenkinsDockerApplication.java
+++ src/main/java/com/pnk/githubjenkinsdocker/GithubJenkinsDockerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubJenkinsDockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubJenkinsDockerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GithubJenkinsDockerApplication.class, args);
+    }
 
 }
