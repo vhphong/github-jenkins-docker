5ea5e4148fd9a51d2090173b5b2fcd4ac7096114
Phong Vo <vhphong@gmail.com>
2024-01-11 18:49:40 -0600

    edited GithubJenkinsDockerApplication

--- src/main/java/com/pnk/githubjenkinsdocker/GithubJenkinsDockerApplication.java
+++ src/main/java/com/pnk/githubjenkinsdocker/GithubJenkinsDockerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 @RequestMapping("/github-jenkins-docker")
