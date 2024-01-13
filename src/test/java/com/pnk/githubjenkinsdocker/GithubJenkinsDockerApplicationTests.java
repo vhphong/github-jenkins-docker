88af9c6a58e8366fee4781635e2fc526866f45d7
Phong Vo <vhphong@gmail.com>
2024-01-12 21:18:22 -0600

    edited GithubJenkinsDockerApplicationTests

--- src/test/java/com/pnk/githubjenkinsdocker/GithubJenkinsDockerApplicationTests.java
+++ src/test/java/com/pnk/githubjenkinsdocker/GithubJenkinsDockerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GithubJenkinsDockerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
